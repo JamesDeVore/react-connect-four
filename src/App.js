102c92224811e4988a948882100b40b4db1aed3b
James DeVore <jdevore4592@gmail.com>
2018-11-27 16:58:21 -0500

    beginning basic board construction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+    constructor(){
+      super()
+      this.state = Array(16).fill(null)
+    }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="board">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+        <div className="row">
+        <button className="move">Here</button>
+        <button className="move">Here</button>
+        <button className="move">Here</button>
+        <button className="move">Here</button>
+        </div>
       </div>
     );
   }
