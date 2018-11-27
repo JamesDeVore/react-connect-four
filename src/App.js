a2ef8bddc05a3bf086f032d53208c421e5c973bb
James DeVore <jdevore4592@gmail.com>
2018-11-27 18:01:50 -0500

    basic construction, and components made

--- src/App.js
+++ src/App.js
@@ -1,47 +1,63 @@
 import React, { Component } from 'react';
-
+import Square from './Square';
 import './App.css';
 
 class App extends Component {
+  state = {
+    squares: [],
+    isRedNext:false
+  }
 
-    constructor(){
-      super()
-      this.state = Array(16).fill(null)
+    handleClick = () => {
+      if(this.state.isRedNext){
+        this.setState({squares:this.state.squares.concat(['blue'])})
+        this.setState({isRedNext:!this.state.isRedNext})
+      }
+      else {
+        this.setState({squares:this.state.squares.concat(['red'])})
+        this.setState({isRedNext:!this.state.isRedNext})
+      }
     }
+
+    renderSquare = (i) => (
+      <Square color={this.state.squares[i]}/>
+    )
 
 
   render() {
     return (
       <div className="board">
+
+
         <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
         </div>
         <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
         </div>
         <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
         </div>
         <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
         </div>
         <div className="row">
-        <button className="move">Here</button>
-        <button className="move">Here</button>
-        <button className="move">Here</button>
-        <button className="move">Here</button>
+        <button className="move" onClick={this.handleClick}>Here</button>
+        <button className="move" onClick={this.handleClick}>Here</button>
+        <button className="move" onClick={this.handleClick}>Here</button>
+        <button className="move" onClick={this.handleClick}>Here</button>
         </div>
       </div>
     );
---
+++ src/Square.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Square = ({ color }) => (
+  <div className={color} className="col">
+  </div>
+)
+
+
+export default Square;
